6faf80d5ecc8e1377ad00509b487da2212f15efb
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2024-03-18 17:51:01 +0200

    edit hero styles

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -20,10 +20,10 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       <PrismicNextImage
         field={slice.primary.image}
         sizes="100vw"
-        className="w-full max-w-[100px] max-h-full rounded-md object-cover"
+        className="w-full max-w-screen-sm max-h-full rounded-md object-cover"
       />
       <div className="flex flex-col gap-2">
-        <h1 className="font-bold text-4xl">
+        <h1 className="font-bold text-4xl my-4">
           <PrismicText field={slice.primary.title} />
         </h1>
         <RichText field={slice.primary.description} />
