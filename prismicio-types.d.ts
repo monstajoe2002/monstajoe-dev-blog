05b8352abc8c3fcd3b9cbe3a35540729407b7017
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2024-03-18 16:46:11 +0200

    remove header slice from blogs

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -4,7 +4,7 @@ import type * as prismic from "@prismicio/client";
 
 type Simplify<T> = { [KeyType in keyof T]: T[KeyType] };
 
-type BlogPostDocumentDataSlicesSlice = HeroSlice | RichTextSlice;
+type BlogPostDocumentDataSlicesSlice = RichTextSlice;
 
 /**
  * Content for Blog Post documents
