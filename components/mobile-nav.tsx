5ca1d0fec5fc08dd41acca31a73c1202348605d1
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2024-03-18 21:53:19 +0200

    add mobile nav menu

---
+++ components/mobile-nav.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+export function MobileNav({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Sheet>
+        <SheetTrigger className={buttonVariants({ variant: "outline" })}>
+          <Menu className="mr-2" />
+          <span>Menu</span>
+        </SheetTrigger>
+        <SheetContent side={"left"}>
+          <div>{children}</div>
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+}
--- components/prismic/navigation.tsx
+++ components/prismic/navigation.tsx
@@ -2,9 +2,10 @@
 
 import { Client, Content, isFilled } from "@prismicio/client";
 import { PrismicLink } from "@prismicio/react";
-import { Button } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
 import { ThemeToggle } from "../theme-toggle";
 import Link from "next/link";
+import { MobileNav } from "@/components/mobile-nav";
 
 export const Navigation = async ({
   client,
@@ -14,21 +15,50 @@ export const Navigation = async ({
   const navigation = await client.getSingle("navigation");
 
   return (
-    <nav className="font-bold text-xl self-center flex w-full">
-      <ul className="flex">
-        {isFilled.group(navigation.data.menu_items) &&
-          navigation.data.menu_items.map((item) => {
-            return (
-              <li key={item.label}>
-                <Button asChild variant={"link"} className="text-xl font-bold">
-                  <PrismicLink field={item.link}>{item.label}</PrismicLink>
-                </Button>
-              </li>
-            );
-          })}
-      </ul>
-
+    <div className="flex justify-between w-full">
+      <nav className="hidden font-bold text-xl self-center md:flex">
+        <ul className="flex">
+          {isFilled.group(navigation.data.menu_items) &&
+            navigation.data.menu_items.map((item) => {
+              return (
+                <li key={item.label}>
+                  <PrismicLink
+                    className={buttonVariants({
+                      variant: "link",
+                      className: "font-bold text-xl",
+                    })}
+                    field={item.link}
+                  >
+                    {item.label}
+                  </PrismicLink>
+                </li>
+              );
+            })}
+        </ul>
+      </nav>
+      <div className="md:hidden visible">
+        <MobileNav>
+          <ul className="flex">
+            {isFilled.group(navigation.data.menu_items) &&
+              navigation.data.menu_items.map((item) => {
+                return (
+                  <li key={item.label}>
+                    <PrismicLink
+                      className={buttonVariants({
+                        variant: "link",
+                        className: "my-4 font-bold text-xl",
+                      })}
+                      field={item.link}
+                    >
+                      {item.label}
+                    </PrismicLink>
+                  </li>
+                );
+              })}
+          </ul>
+        </MobileNav>
+      </div>
       <ThemeToggle />
-    </nav>
+    </div>
   );
 };
