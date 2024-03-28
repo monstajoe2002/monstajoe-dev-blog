4b3c8b8a55b9aa230ae4c41d1ff527fd435ec870
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2024-03-28 16:04:31 +0200

    revalidate data after 5 minutes in production

--- prismicio.ts
+++ prismicio.ts
@@ -35,10 +35,15 @@ const routes: prismic.ClientConfig["routes"] = [
 export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
   const client = prismic.createClient(repositoryName, {
     routes,
-    fetchOptions:
-      process.env.NODE_ENV === "production"
-        ? { next: { tags: ["prismic"] }, cache: "force-cache" }
-        : { next: { revalidate: 5 } },
+    fetchOptions: {
+      next: {
+        revalidate: 300,
+        tags: ["prismic"],
+      },
+    },
+    // process.env.NODE_ENV === "production"
+    //   ? { next: { tags: ["prismic"] }, cache: "force-cache" }
+    //   : { next: { revalidate: 5 } },
     ...config,
   });
 
