7d3952fa7fab003e3e1015c49c5a196df96374a6
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2024-04-26 14:47:12 +0300

    remove unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Fira_Sans, Inter, Noto_Sans, Poppins } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import "./globals.css";
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
